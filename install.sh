86ef2461e4435c3134844f56bd1be334138a502b
Harin <iamharinramesh@gmail.com>
2024-11-23 00:45:08 +0530

    updated install script

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-ln -sf ~/working_dir/dotfiles/.conf/nvim ~/.config/nvim
+CURRENT_DIR=$(pwd)
+CONFIG_PATH="$CURRENT_DIR/.conf/"
+
+ln -sf "$CONFIG_PATH/nvim" ~/.config/nvim
+ln -sf "$CONFIG_PATH/kitty" ~/.config/kitty
+ln -sf "$CONFIG_PATH/yazi" ~/.config/yazi
+ln -sf "$CONFIG_PATH/starship.toml" ~/.config/starship.toml
+
+ln -sf "$CURRENT_DIR/.tmux.conf" ~/.tmux.conf
