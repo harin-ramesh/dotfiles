9c2b689cd6e08a788a8bb8b21ed6f78a7eca74e7
Harin <iamharinramesh@gmail.com>
2025-12-14 17:26:02 +0530

    Update snacks.lua

--- .conf/nvim/lua/plugins/snacks.lua
+++ .conf/nvim/lua/plugins/snacks.lua
@@ -22,25 +22,7 @@ return {
   },
   keys = {
     { "<leader>e", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
-    { "<leader>lg", function() Snacks.lazygit() end, desc = "Lazygit" },
-    {
-      "<leader>N",
-      desc = "Neovim News",
-      function()
-        Snacks.win({
-          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = "yes",
-            statuscolumn = " ",
-            conceallevel = 3,
-          },
-        })
-      end,
-    }
+    { "<leader>lg", function() Snacks.lazygit() end, desc = "Lazygit" }
   },
   config = function(_, opts)
     require("snacks").setup(opts) 
