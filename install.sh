fcbde34d800af451968d5966cb3f597666707582
Harin <iamharinramesh@gmail.com>
2026-01-26 18:26:09 +0530

    updated install sript

--- install.sh
+++ install.sh
@@ -1,24 +1,60 @@
+#!/bin/bash
+
 CURRENT_DIR=$(pwd)
 CONFIG_PATH="$CURRENT_DIR/.conf"
 
-ln -sf "$CONFIG_PATH/nvim" ~/.config/nvim
-ln -sf "$CONFIG_PATH/kitty" ~/.config/kitty
-ln -sf "$CONFIG_PATH/yazi" ~/.config/yazi
-ln -sf "$CONFIG_PATH/i3" ~/.config/i3
-ln -sf "$CONFIG_PATH/rofi" ~/.config/rofi
-ln -sf "$CONFIG_PATH/polybar" ~/.config/polybar
-ln -sf "$CONFIG_PATH/picom" ~/.config/picom
-ln -sf "$CONFIG_PATH/fish" ~/.config/fish
-ln -sf "$CONFIG_PATH/starship.toml" ~/.config/starship.toml
-ln -sf "$CONFIG_PATH/wezterm" ~/.config/wezterm
+# Safe symlink creator
+link() {
+    src="$1"
+    dest="$2"
 
-ln -sf "$CURRENT_DIR/.tmux.conf" ~/.tmux.conf
+    # If destination is a symlink and correct, skip
+    if [ -L "$dest" ] && [ "$(readlink "$dest")" = "$src" ]; then
+        echo "OK: $dest already -> $src"
+        return
+    fi
 
+    # If destination exists but is wrong, remove it
+    if [ -e "$dest" ] || [ -L "$dest" ]; then
+        echo "Removing old $dest"
+        rm -rf "$dest"
+    fi
 
-# Create symlinks for Neovim configuration in root's home directory
+    echo "Linking: $dest -> $src"
+    ln -s "$src" "$dest"
+}
+
+##############################
+# USER CONFIG SYMLINKS
+##############################
+
+link "$CONFIG_PATH/nvim"           "$HOME/.config/nvim"
+link "$CONFIG_PATH/kitty"          "$HOME/.config/kitty"
+link "$CONFIG_PATH/yazi"           "$HOME/.config/yazi"
+link "$CONFIG_PATH/i3"             "$HOME/.config/i3"
+link "$CONFIG_PATH/rofi"           "$HOME/.config/rofi"
+link "$CONFIG_PATH/polybar"        "$HOME/.config/polybar"
+link "$CONFIG_PATH/picom"          "$HOME/.config/picom"
+link "$CONFIG_PATH/fish"           "$HOME/.config/fish"
+link "$CONFIG_PATH/starship.toml"  "$HOME/.config/starship.toml"
+link "$CONFIG_PATH/wezterm"        "$HOME/.config/wezterm"
+
+link "$CURRENT_DIR/.tmux.conf"     "$HOME/.tmux.conf"
+
+##############################
+# ROOT CONFIG SYMLINKS
+##############################
+
+# Create necessary directories
 sudo mkdir -p /root/.config
-sudo ln -sf /home/$USER/.config/nvim /root/.config/nvim
+sudo mkdir -p /root/.local/share
+sudo mkdir -p /root/.local/state
+sudo mkdir -p /root/.cache
 
-sudo ln -sf /home/$USER/.local/share/nvim /root/.local/share/nvim
-sudo ln -sf /home/$USER/.local/state/nvim /root/.local/state/nvim
-sudo ln -sf /home/$USER/.cache/nvim /root/.cache/nvim
+# Run link() inside sudo context
+sudo bash -c "$(declare -f link); link \"/home/$USER/.config/nvim\" \"/root/.config/nvim\""
+sudo bash -c "$(declare -f link); link \"/home/$USER/.local/share/nvim\" \"/root/.local/share/nvim\""
+sudo bash -c "$(declare -f link); link \"/home/$USER/.local/state/nvim\" \"/root/.local/state/nvim\""
+sudo bash -c "$(declare -f link); link \"/home/$USER/.cache/nvim\" \"/root/.cache/nvim\""
+
+echo "Install complete."
